20bd271de96f40aad4af0d63d4f3cd37e3c85576
Le Dinh Quyen <quyenld9699@gmail.com>
2024-08-23 01:03:25 +0700

    update data

--- src/pages/your-campaigns/ListCampaigns/ListCampaigns.tsx
+++ src/pages/your-campaigns/ListCampaigns/ListCampaigns.tsx
@@ -1,4 +1,4 @@
-import { Campaign, CardCampaign, getCampaigns, NoData } from '@auxo-dev/frontend-common';
+import { Campaign, CardCampaign, getCampaigns, IconSpinLoading, NoData } from '@auxo-dev/frontend-common';
 import { Box, Grid } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
@@ -6,6 +6,7 @@ import { useAccount } from 'wagmi';
 export default function ListCampaigns() {
     const { address } = useAccount();
     const [data, setData] = useState<Campaign[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
     async function fetchCourses(addressUser: string) {
         try {
             const response = await getCampaigns();
@@ -14,14 +15,23 @@ export default function ListCampaigns() {
             console.error(error);
             setData([]);
         }
+        setLoading(false);
     }
     useEffect(() => {
         if (address) {
             fetchCourses(address);
         }
     }, [address]);
+
+    if (loading) {
+        return (
+            <Box>
+                <IconSpinLoading sx={{ fontSize: '100px' }} />
+            </Box>
+        );
+    }
     return (
-        <Box>
+        <Box mt={3}>
             <Box textAlign={'center'}>{data.length === 0 && <NoData />}</Box>
             <Box>
                 <Grid container spacing={2}>
